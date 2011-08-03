b2dd764a75a2694ea48612f7d57c7516e9609722
Yuki Kimoto <kimoto.yuki@gmail.com>
2011-08-03 15:46:57 +0900

    many changed

---
+++ example.pl
@@ -0,0 +1,120 @@
+use Mojolicious::Lite;
+use utf8;
+
+get '/' => 'index';
+
+app->start;
+
+__DATA__
+
+@@ index.html.ep
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>DateSelectSimple.js Example1</title>
+    
+    %= javascript '/js/jquery.js'
+    %= javascript '/js/DateSelectSimple.js';
+    
+    <%= javascript begin %>
+      $(document).ready(function () {
+        // Simple
+        var dss1 = DateSelectSimple.create();
+        $("#date_select1").append(dss1.component());
+        
+        // Customize1
+        var dss2 = DateSelectSimple.create({
+          size: 5, name: "due_date", year: 2008, month: 12});
+        $("#date_select2").append(dss2.component());
+        
+        // Customize2
+        var dss3 = DateSelectSimple.create({size: 5, empty_item: true});
+        $("#date_select3").append(dss3.component());
+
+        // Customize3
+        var dss4 = DateSelectSimple.create({
+          caption_format: "%Y/%m",
+          text_format: "%Y/%m/%d(%a)",
+          value_format: "%Y/%m/%d"
+        });
+        $("#date_select4").append(dss4.component());
+
+        // Japanese
+        var dss_jp = DateSelectSimple.create({
+          days: ["日", "月", "火", "水", "木", "金", "土"],
+          caption_format: "%Y月%m日",
+          text_format: "%Y/%m/%d(%a)",
+          value_format: "%Y/%m/%d"
+        });
+        $("#date_select_jp").append(dss_jp.component());
+
+        // Change callback
+        var dss_cb = DateSelectSimple.create({size: 5});
+        dss_cb.change(function (select) {
+          var selected = select.children(":selected");
+          alert(selected.val());
+        });
+        $("#date_select_cb").append(dss_cb.component());
+
+        // Popup
+        var dss_popup = DateSelectSimple.create();
+        var popup = $("<div/>");
+        popup.css("width", "150px")
+          .css("position", "absolute")
+          .css("border", "1px black solid")
+          .css("background-color", "white");
+        dss_popup.change(function (select) {
+          var selected = select.children(":selected");
+          $('input[name="date_input"]').val(selected.val());
+          popup.hide();
+        });
+        var popup_exists;
+        $('input[name="date_button"]').click(function (event) {
+          if (!popup_exists) {
+            $(this).parent().append(popup);
+            popup_exists = true;
+          }
+          popup.css("left", event.pageX)
+            .css("top", event.pageY)
+            .show();
+        });
+        popup.append(dss_popup.component());
+      });
+    <% end %>
+    
+  </head>
+  <body>
+    <h3>Simple</h3>
+    <p>default setting</p>
+    <div id="date_select1"></div>
+
+    <h3>Castamize1</h3>
+    <p>Set select field size and name, and default year and month</p>
+    <div id="date_select2"></div>
+
+    <h3>Castamize2</h3>
+    <p>Added empty_item</p>
+    <div id="date_select3"></div>
+
+    <h3>Castamize3</h3>
+    <p>Change date format</p>
+    <div id="date_select4"></div>
+
+    <h3>Japanese</h3>
+    <p>Change date format</p>
+    <div id="date_select_jp"></div>
+
+    <h3>Change callback</h3>
+    <p>Register item change callback</p>
+    <div id="date_select_cb"></div>
+
+    <h3>Change callback</h3>
+    <p>Register item change callback</p>
+    <div><input name="date_input"><input type="button" name="date_button"value="..."></div>
+    
+    <br>
+    <br>
+    <br>
+    
+  </body>
+</html>
---
+++ public/js/DateSelectSimple.js
@@ -0,0 +1,202 @@
+(
+  function () {
+    if (window.DateSelectSimple) { return }
+    var proto = window.DateSelectSimple = {};
+
+    // Attributes
+    var attrs = ["change", "caption_format", "days", "empty_item", "month",
+      "name", "size", "text_format", "value_format", "year"];
+    for (var i = 0; i < attrs.length; i++) {
+      var attr = attrs[i];
+      proto[attr] = (function () {
+        var attr = attrs[i];
+        return function () {
+          if (arguments.length) {
+            this["_" + attr] = arguments[0];
+            return this;
+          }
+          return this["_" + attr];
+        }
+      }());
+    }
+     
+    // Methods
+    proto.component = function () {
+        if (!this._component) { this._component = this._create_component() }
+        return this._component;
+    };
+
+    proto.create = function (args) {
+      var obj = {}
+      for (var name in proto) { obj[name] = proto[name] }
+      obj._init(args);
+      return obj;
+    };
+    
+    proto._init = function (args) {
+      this.name("date");
+      this.change(function () {});
+      this.size(1);
+      this.empty_item(false);
+      this.caption_format("%Y-%m");
+      this.text_format("%Y-%m-%d(%a)");
+      this.value_format("%Y-%m-%d");
+      this.days(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]);
+      this._date_select = $("<select/>");
+      var today = new Date();
+      this.year(today.getFullYear());
+      this.month(today.getMonth() + 1);
+      for (name in args) { this["_" + name] = args[name] }
+    };
+    
+    proto._add_month = function (add_month) {
+      this.month(this.month() + add_month);
+      if (this.month() < 1) {
+        this.month(12);
+        this.year(this._year - 1);
+      }
+      else if (this.month() > 12) {
+        this.month(1);
+        this.year(this._year + 1);
+      }
+      return this;
+    };
+
+    proto._last_mday = function (year, month) {
+    	var next_year;
+    	var next_month;
+    	if (month == 12){
+    		next_year = year + 1;
+    		next_month = 1;
+    	}
+    	else{
+    		next_year = year;
+    		next_month = month + 1;
+    	}
+    	var last_date = new Date(next_year, next_month - 1, 0);
+    	return last_date.getDate();
+    };
+
+    proto._caption = function () {
+      return this._format(this.caption_format(), this.year(), this.month());
+    };
+    
+    proto._create_component = function () {
+      var that = this;
+      
+      // Container
+      var container = $("<div/>");
+      container.css("text-align", "center")
+        .css("width", "140px");
+      
+      // Display year and display
+      var table = $('<table align="center"/>');
+      table.css("width", "90%")
+        .css("border", "none");
+      var tr = $('<tr/>');
+      tr.css("border", "none");
+      var td_left = $('<td/>');
+      var td_center = $('<td/>');
+      var td_right = $('<td/>');
+      td_center.text(this._caption())
+      td_center.css("text-align", "center")
+        .css("border", "none");
+      tr.append(td_left);
+      tr.append(td_center);
+      tr.append(td_right);
+      table.append(tr);
+      
+      // Event
+      var navi_effect = {
+        in: function() {
+          $(this).css("text-decoration", "underline")
+           .css("cursor", "hand")
+        },
+        out: function () {
+          $(this).css("text-decoration", "none");
+        }
+      };
+      var month_forward = function(add) {
+          that._add_month(add);
+          td_center.text(that._caption());
+          that._update_date_select();
+          return false;
+      };
+      
+      // Privious month
+      td_left.html('<a href="#">&lt&lt</a>');
+      td_left.css("text-align", "left")
+        .css("border", "none");
+      td_left.children("a").css("text-decoration", "none")
+        .hover(navi_effect.in, navi_effect.out)
+        .click(function () { return month_forward(-1) });
+      
+      // Next month
+      td_right.html('<a href="#">&gt&gt</a>');
+      td_right.css("text-align", "right")
+        .css("border", "none");
+      td_right.children("a").css("text-decoration", "none")
+        .hover(navi_effect.in, navi_effect.out)
+        .click(function () { return month_forward(1) });
+      container.append(table);
+      
+      // Date selection
+      container.append(this._date_select);
+      var date_select = this._date_select;
+      date_select.attr("name", this.name());
+      date_select.attr("size", this.size());
+      date_select.change(function () { that.change()($(this)) });
+      this._update_date_select();
+      
+      return container;
+    };
+
+    proto._day = function (year, month, mday) {
+      var days = this.days();
+      var date = new Date(year, month - 1, mday);
+      var day = days[date.getDay()];
+      return day;
+    }
+    
+    proto._format = function (format, year, month, mday) {
+      var date = format;
+      if (year) {date = date.replace(/%Y/, year) }
+      if (month) {
+        var fmonth = String(month).length === 1 ? "0" + month : "" + month;
+        date = date.replace(/%m/, fmonth);
+      }
+      if (mday) {
+        var fmday = String(mday).length == 1 ? "0" + mday : "" + mday;
+        date = date.replace(/%d/, fmday);
+        date = date.replace(/%a/, this._day(year, month, mday));
+      }
+      return date;
+    };
+    
+    proto._option_text = function (mday) {
+      return this._format(this.text_format(), this.year(), this.month(), mday);
+    };
+    
+    proto._option_value = function (mday) {
+      return this._format(this.value_format(), this.year(), this.month(), mday);
+    };
+
+    proto._update_date_select = function () {
+      var date_select = this._date_select;
+      
+      date_select.empty();
+      if (this.empty_item()) { date_select.append('<option value=""/>') }
+      var last_mday  = this._last_mday(this.year(), this.month());
+      for (var mday = 1; mday <= last_mday; mday++) {
+        var date = new Date(this.year(), this.month() - 1, mday);
+        var day = this.days()[date.getDay()];
+        
+        var option = $("<option/>");
+        option.val(this._option_value(mday));
+        option.text(this._option_text(mday));
+        date_select.append(option);
+      }
+    };
+    
+  }
+)();
